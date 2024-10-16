bcc4858481232f31bd9e37ef0702a97a81f3c287
Mohammed Shareef <shareef3533@gmail.com>
2024-10-16 17:18:05 -0400

    Font

--- backend/models/user.models.js
+++ backend/models/user.models.js
@@ -1,13 +1,6 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-<<<<<<< HEAD
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    isPremium: { type: Boolean, default: false }, // Add premium field if required
-    createdAt: { type: Date, default: Date.now }
-=======
   name: {type: String, required: true},
   email: {type: String, required: true, unique: true},
   password: {type: String, required: true},
@@ -17,7 +10,6 @@ const userSchema = new mongoose.Schema({
   createdAt: {type: Date, default: Date.now},
   resetPasswordToken: { type: String , default: null }, 
   resetPasswordExpires: { type: Date , default : null },   
->>>>>>> c378ee18113c8ca0aa00d28a645568ab9c324ade
 });
 
 module.exports = mongoose.model('User', userSchema);
